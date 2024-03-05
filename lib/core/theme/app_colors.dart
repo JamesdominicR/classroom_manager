2ca0b28e902e1de410632bc8d4f1cca988221400
Sanket Gourai <58967280+JamesdominicR@users.noreply.github.com>
2024-03-05 22:44:16 +0530

    Initial commit

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor{
+  static const Color primaryColor = Colors.white;
+  static const Color containerPrimaryBlue = Color(0xFFAAC9BF);
+  static const Color containerSecondaryBlue = Color(0xFFD8EBFD);
+  static const Color containerRose = Color(0xFFFFE0DD);
+  static const Color containerPink = Color(0xFFFFF3D9);
+  static const Color cardColor = Color(0xFFD1D1D1);
+  static const Color primaryButtonColor = Color(0xFF0FAB76);
+  static const Color secondaryButtonColor = Color(0xFFAAC9BF);
+  static const Color redColor = Color(0xFFF9614F);
+}
